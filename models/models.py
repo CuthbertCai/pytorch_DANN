b67685f0b8db070b47f7e0016c33b13a30130787
tengerye <yetengqi@gmail.com>
2018-07-25 14:57:10 +0800

    restructure and add source only and target only training

---
+++ models/models.py
@@ -0,0 +1,72 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+class GradReverse(torch.autograd.Function):
+    """
+    Extension of grad reverse layer
+    """
+    @staticmethod
+    def forward(ctx, x, constant):
+        ctx.constant = constant
+        return x.view_as(x)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        grad_output = grad_output.neg() * ctx.constant
+        return grad_output, None
+
+    def grad_reverse(x, constant):
+        return GradReverse.apply(x, constant)
+
+class Extractor(nn.Module):
+
+    def __init__(self):
+        super(Extractor, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size= 5)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(64, 50, kernel_size= 5)
+        self.bn2 = nn.BatchNorm2d(50)
+        self.conv2_drop = nn.Dropout2d()
+
+    def forward(self, input):
+        input = input.expand(input.data.shape[0], 3, 28, 28)
+        x = F.relu(F.max_pool2d(self.bn1(self.conv1(input)), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.bn2(self.conv2(x))), 2))
+        x = x.view(-1, 50 * 4 * 4)
+
+        return x
+
+class Class_classifier(nn.Module):
+
+    def __init__(self):
+        super(Class_classifier, self).__init__()
+        self.fc1 = nn.Linear(50 * 4 * 4, 100)
+        self.bn1 = nn.BatchNorm1d(100)
+        self.fc2 = nn.Linear(100, 100)
+        self.bn2 = nn.BatchNorm1d(100)
+        self.fc3 = nn.Linear(100, 10)
+
+    def forward(self, input):
+        logits = F.relu(self.bn1(self.fc1(input)))
+        logits = self.fc2(F.dropout(logits))
+        logits = F.relu(self.bn2(logits))
+        logits = self.fc3(logits)
+
+        return F.log_softmax(logits)
+
+class Domain_classifier(nn.Module):
+
+    def __init__(self):
+        super(Domain_classifier, self).__init__()
+        self.fc1 = nn.Linear(50 * 4 * 4, 100)
+        self.bn1 = nn.BatchNorm1d(100)
+        self.fc2 = nn.Linear(100, 2)
+
+    def forward(self, input, constant):
+        input = GradReverse.grad_reverse(input, constant)
+        logits = F.relu(self.bn1(self.fc1(input)))
+        logits = F.log_softmax(self.fc2(logits))
+
+        return logits
