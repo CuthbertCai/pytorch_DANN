ea1289d05892bb4940a918a1bee98eb1b894e20f
yetengqi <yet2@silvrr.com>
2018-07-25 15:07:23 +0800

    restructure

--- params.py
+++
@@ -1,8 +0,0 @@
-use_gpu = False
-data_root = './data'
-dataset_mean = (0.5, 0.5, 0.5)
-dataset_std = (0.5, 0.5, 0.5)
-source_path = data_root + '/MNIST'
-target_path = data_root + '/mnist_m'
-batch_size = 512
-epochs = 100
