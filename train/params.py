cb68823b8925f8ea27bd34441c70eb4869f36fee
tengerye <yetengqi@gmail.com>
2018-11-15 09:34:50 +0800

    Local parameters.

--- train/params.py
+++ train/params.py
@@ -1,8 +1,8 @@
-use_gpu = True
+use_gpu = False
 data_root = './data'
 dataset_mean = (0.5, 0.5, 0.5)
 dataset_std = (0.5, 0.5, 0.5)
-source_path = data_root + '/MNIST'
-target_path = data_root + '/MNIST_M'
+source_path = data_root + '/MNIST/'
+target_path = data_root + '/mnist_m'
 batch_size = 512
 epochs = 100
